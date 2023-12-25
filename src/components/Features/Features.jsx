261ab64ed58fb529a970770ba8a09bed2a27b579
Emmanuel Kumah <mailkumah@gmail.com>
2023-12-25 19:36:26 +0000

    Updated UseCase component

---
+++ src/components/Features/Features.jsx
@@ -0,0 +1,18 @@
+import { Typography, Box, styled } from "@mui/material";
+import React from "react";
+
+const Features = () => {
+  return (
+    <>
+      <Box>
+        <Typography variant="h3">Features</Typography>
+        <Typography variant="subTitle1">
+          What is under the hood of our voice recognition service
+        </Typography>
+      </Box>
+      Features
+    </>
+  );
+};
+
+export default Features;
