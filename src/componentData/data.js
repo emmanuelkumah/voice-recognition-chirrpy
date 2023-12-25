73f31c5cbb27281109a946f6085b2133bde00170
Emmanuel Kumah <mailkumah@gmail.com>
2023-12-25 22:25:12 +0000

    Added Features section

---
+++ src/componentData/data.js
@@ -0,0 +1,51 @@
+import downloadIcon from "../assets/images/download.png";
+import smileyIcon from "../assets/images/smiley.png";
+import chatIcon from "../assets/images/chat.png";
+import editIcon from "../assets/images/edit.png";
+import usersIcon from "../assets/images/users.png";
+import translateIcon from "../assets/images/Languages.png";
+import entityIcon from "../assets/images/user.png";
+import wordIcon from "../assets/images/word.png";
+
+export const featuresData = [
+  {
+    id: 1,
+    description: `Download Transcript As PDF`,
+    imageUrl: downloadIcon,
+  },
+  {
+    id: 2,
+    description: `Sentiment Analysis`,
+    imageUrl: smileyIcon,
+  },
+  {
+    id: 3,
+    description: `Topic Detection`,
+    imageUrl: chatIcon,
+  },
+  {
+    id: 4,
+    description: `Summarization`,
+    imageUrl: editIcon,
+  },
+  {
+    id: 5,
+    description: `Detect Speakers`,
+    imageUrl: usersIcon,
+  },
+  {
+    id: 6,
+    description: `Translate Text`,
+    imageUrl: translateIcon,
+  },
+  {
+    id: 7,
+    description: `Entity Detection`,
+    imageUrl: entityIcon,
+  },
+  {
+    id: 8,
+    description: `Download Word`,
+    imageUrl: wordIcon,
+  },
+];
--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,16 +1,49 @@
-import { Typography, Box, styled } from "@mui/material";
-import React from "react";
+import { Typography, Box, Container, Grid, styled } from "@mui/material";
+import { theme } from "../../theme";
+import FeaturesCard from "../UI/FeaturesCard";
+import { featuresData } from "../../componentData/data";
 
 const Features = () => {
+  const StyledSectionBackground = styled(Box)(({ theme }) => ({
+    background: theme.palette.primary.main,
+    paddingBottom: "2rem",
+  }));
+
+  const StyledTypography = styled(Typography)(({ theme }) => ({
+    color: "#fff",
+    fontFamily: theme.typography.heading.fontFamily,
+    fontSize: theme.typography.subHeading.fontSize,
+    fontWeight: theme.typography.heading.fontWeight,
+    textAlign: "center",
+    paddingTop: "2rem",
+    paddingBottom: "1rem",
+  }));
+
+  const StyledDescription = styled(Typography)(({ theme }) => ({
+    color: "#fff",
+    display: "grid",
+    placeItems: "center",
+    paddingTop: "1rem",
+    paddingBottom: "1rem",
+  }));
+
   return (
     <>
-      <Box>
-        <Typography variant="h3">Features</Typography>
-        <Typography variant="subTitle1">
-          What is under the hood of our voice recognition service
-        </Typography>
-      </Box>
-      Features
+      <StyledSectionBackground>
+        <Container>
+          <StyledTypography variant="h3">Features</StyledTypography>
+          <StyledDescription variant="subTitle1">
+            What is under the hood of our voice recognition service
+          </StyledDescription>
+          <Grid container spacing={2}>
+            {featuresData.map((item) => (
+              <Grid item xs={12} sm={3}>
+                <FeaturesCard item={item} key={item.id} />
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </StyledSectionBackground>
     </>
   );
 };
