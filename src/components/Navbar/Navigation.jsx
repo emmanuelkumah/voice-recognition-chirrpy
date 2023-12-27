ea8535014f0ceaace56ba5056a2d77ed610e8d73
Emmanuel Kumah <mailkumah@gmail.com>
2023-12-27 23:23:17 +0000

    Fix responsive nav menus

--- src/components/Navbar/Navigation.jsx
+++ src/components/Navbar/Navigation.jsx
@@ -8,9 +8,7 @@ import {
   Menu,
   styled,
   MenuItem,
-  Paper,
   Typography,
-  Button,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { theme } from "../../theme";
@@ -21,7 +19,6 @@ const Navigation = () => {
   const [anchorElNav, setAnchorElNav] = useState(null);
 
   const handleNavMenuOpen = (event) => {
-    console.log(event.currentTarget);
     setAnchorElNav(event.currentTarget);
   };
   const handleCloseNavMenu = () => {
@@ -30,8 +27,6 @@ const Navigation = () => {
 
   const StyledAppBar = styled(AppBar)(({ theme }) => ({
     height: "80px",
-
-    [theme.breakpoints.up("sm")]: {},
   }));
 
   const StyledToolBar = styled(Toolbar)(({ theme }) => ({
@@ -68,6 +63,56 @@ const Navigation = () => {
                 </MenuItem>
               ))}
             </StyledNavMenu>
+            {/* Responsive menu */}
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: "flex", md: "none" },
+                justifyContent: { xs: "flex-end", md: "none" },
+              }}
+            >
+              <IconButton
+                size="large"
+                aria-label="navigation menu"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleNavMenuOpen}
+              >
+                <MenuIcon sx={{ color: theme.palette.primary.main }} />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                  position: "absolute",
+                  top: "60px",
+                }}
+              >
+                {navItems.map((menu) => (
+                  <MenuItem
+                    key={menu}
+                    onClick={handleCloseNavMenu}
+                    sx={{ width: "100vw" }}
+                  >
+                    <Typography textAlign="center" variant="navItems">
+                      {menu}
+                    </Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
           </StyledToolBar>
         </Container>
       </StyledAppBar>
