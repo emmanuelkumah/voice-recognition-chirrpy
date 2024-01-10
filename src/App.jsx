4495042c98cb4369502bed0e5fc8aa482d40cd18
Emmanuel Kumah <mailkumah@gmail.com>
2024-01-10 08:37:37 +0000

    Clean up App.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
-import { AudioRecorder, useAudioRecorder } from "react-audio-voice-recorder";
+import { useState, useEffect } from "react";
+import { Recorder } from "react-voice-recorder";
+import { AssemblyAI } from "assemblyai";
+
+// import "react-voice-recorder/dist/index.css"; //
+// import { AudioRecorder, useAudioRecorder } from "react-audio-voice-recorder";
 
 function App() {
-  const recorderControls = useAudioRecorder(
-    {
-      noiseSuppression: true,
-      echoCancellation: true,
-    },
-    (err) => console.table(err) // onNotAllowedOrFound
-  );
-  const addAudioElement = (blob) => {
-    const url = URL.createObjectURL(blob);
-    const audio = document.createElement("audio");
-    audio.src = url;
-    audio.controls = true;
-    document.body.appendChild(audio);
-  };
   return (
     <>
       <h3>This is the app page</h3>
-      <AudioRecorder
-        onRecordingComplete={(blob) => addAudioElement(blob)}
-        recorderControls={recorderControls}
-        showVisualizer={true}
-      />
-      <button onClick={recorderControls.stopRecording}>Stop recording</button>
     </>
   );
 }
